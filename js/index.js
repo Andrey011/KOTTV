f90bca1d61f7435334723ecfe122c4be9eb3f5f0
Andrey011 <kudravets001@gmail.com>
2023-01-27 13:46:50 +0300

    Intro_carousel

--- js/index.js
+++ js/index.js
@@ -74,3 +74,74 @@ $('.center').slick({
           }
       ]
       });
+$(".single-item-intro").slick({
+  centerMode: true,
+  vertical: true,
+  centerPadding: '60px',
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  lazyLoad: 'ondemand',
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+          {
+              breakpoint: 768,
+              settings: {
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 3
+              }
+          },
+          {
+          breakpoint: 480,
+          settings: {
+                  centerMode: true,
+                  centerPadding: '40px',
+                  slidesToShow: 1
+              }
+          }
+      ]
+});
+$(".slider-channels").each(function () {
+    for (var i = 0; i < 3; i++) {
+      $(this).find('.slide-channels').clone().appendTo(this);
+    }
+  });
+//$(".single-item-intro").each(function () {
+//    for (var i = 0; i < 3; i++) {
+//      $(this).find('.slide-channels').clone().appendTo(this);
+//    }
+// $('[data-intro-slide]').clone().appendTo('[data-intro="carousel"]');
+//  $('[data-intro-detail="slide"]').clone().appendTo('[data-intro-detail="carousel"]');
+//  $('[data-intro="carousel"]').slick({
+//    centerMode: true,
+//    centerPadding: '0px',
+//    infinite: true,
+//    autoplay: true,
+//    autoplaySpeed: 0,
+//    slidesToShow: 7,
+//    slidesToScroll: 1,
+//    speed: 8000,
+//    asNavFor: '[data-intro-detail="carousel"]',
+//    responsive: [{
+//      breakpoint: 992,
+//      settings: {
+//        slidesToShow: 5
+//      }
+//    },
+//    {
+//      breakpoint: 768,
+//      settings: {
+//        slidesToShow: 3
+//      }
+//    },
+//    ]
+//  });
+//    $('[data-intro-river-carousel]').each(function () {
+//    for (var i = 0; i < 3; i++) {
+//      $(this).find('.channels-slide').clone().appendTo(this);
+//    }
+//  });
+
